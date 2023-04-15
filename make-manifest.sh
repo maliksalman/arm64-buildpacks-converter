72fab778780a6151b89dea2b3d16a1e9f67fe326
Salman Malik <salman@the-tech-warriors.com>
2023-04-15 13:21:41 -0400

    Adding a git commit/push job

---
+++ make-manifest.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+MANIFEST_IMAGE=$1
+CONV_BUILDER=$2
+ORIG_BUILDER=$3
+
+docker manifest inspect $MANIFEST_IMAGE >/dev/null 2>/dev/null
+if [[ $? -eq 0 ]]; then
+    docker manifest rm $MANIFEST_IMAGE
+fi
+
+docker manifest create $MANIFEST_IMAGE \
+    $CONV_BUILDER \
+    $ORIG_BUILDER
